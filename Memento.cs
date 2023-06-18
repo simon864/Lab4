92bec550f689a111f07cb57f92a6a31225e92750
katevolved3@mail.ru <katevolved3@mail.ru>
2023-06-18 23:12:44 +0700

    Implemented the Memento pattern.

---
+++ Memento.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    public class Memento
+    {
+        public string Content { get; set; }
+    }
+    public interface IOriginator
+    {
+        object GetMemento();
+        void SetMemento(object memento);
+    }
+    public class Caretaker
+    {
+        private object memento;
+        public void SaveState(IOriginator originator)
+        {
+            memento = originator.GetMemento();
+        }
+
+        public void RestoreState(IOriginator originator)
+        {
+            originator.SetMemento(memento);
+        }
+    }
+}
