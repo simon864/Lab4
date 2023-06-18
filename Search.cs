dad57a57e889a8948953f79f8ae5f168f7f6886c
katevolved3@mail.ru <katevolved3@mail.ru>
2023-06-18 23:14:41 +0700

    Created a class for searching text files by keywords words.

---
+++ Search.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Search
+    {
+        public static void Searching(string Path, string[] KeyWords)
+        {
+            string[] Files = Directory.GetFiles(Path, "*.txt");
+            foreach (string file in Files)
+            {
+                bool Flag = true;
+                foreach (string KeyWord in KeyWords)
+                {
+                    if (KeyWord != null)
+                    {
+                        if (!File.ReadAllText(file).Contains(KeyWord))
+                        {
+                            Flag = false;
+                        }
+                    }
+                }
+                if (Flag)
+                {
+                    Console.WriteLine(file);
+                }
+            }
+        }
+    }
+}
